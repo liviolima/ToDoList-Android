4181d6a0aa96b09805c0b8fa1eb326af378c04b8
LivioLima <livio.lima.br@gmail.com>
2016-12-26 10:14:16 -0300

    adds MainActivity to go to NewTaskActivity.

--- app/src/main/java/com/livio/todolist/MainActivity.java
+++ app/src/main/java/com/livio/todolist/MainActivity.java
@@ -1,7 +1,9 @@
 package com.livio.todolist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void GoToNewTaskActivity(View v){
+        Intent intent = new Intent(MainActivity.this, NewTaskActivity.class);
+        startActivity(intent);
+    }
+
 }
